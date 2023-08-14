4d6e6f043c10812d123c97bca2912c40eff872a9
seongssu <loveforggong@gmail.com>
2023-08-14 17:17:34 +0900

    [FEAT] : ⚙️ SignInActivity -> SignUp,Main 으로 이동하는 버튼추가, MyPageDetailActivity 추가

--- app/src/main/java/com/android/iz4/SignInActivity.kt
+++ app/src/main/java/com/android/iz4/SignInActivity.kt
@@ -1,11 +1,25 @@
 package com.android.iz4
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signin)
+
+        val btnlogin = findViewById<Button>(R.id.sibtnlogin)
+        val btnjoin = findViewById<Button>(R.id.sibtnjoin)
+
+        btnlogin.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+        btnjoin.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
