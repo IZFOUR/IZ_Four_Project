3219b9cdaf109bac3a8b3396d9e6aaefa7ee8f76
seongssu <loveforggong@gmail.com>
2023-08-14 20:56:35 +0900

    [FEAT] : ⚙️ activity_main.xml 디자인 초안

--- app/src/main/java/com/android/iz4/MyAbilityDetail.kt
+++ app/src/main/java/com/android/iz4/MyAbilityDetail.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class MyAbilityDetail : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_my_ability_detail)
+        setContentView(R.layout.activity_myabilitydetail)
     }
 }
