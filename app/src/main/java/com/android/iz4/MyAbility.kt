b1125eff177fc0d2bf39e76d65ac1130cd9cce5f
seongssu <loveforggong@gmail.com>
2023-08-15 06:09:10 +0900

    [FEAT] : ⚙️ MyAbility 관련 Activitiy, xml 기본 초안

--- app/src/main/java/com/android/iz4/MyAbility.kt
+++ app/src/main/java/com/android/iz4/MyAbility.kt
@@ -1,11 +1,30 @@
 package com.android.iz4
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.Toast
 
+// 조병현 형님: MyTeamPage -> MyAbility로 액티비티 이름 변경하였습니다.
+// Theme에서 Parent부분 Meterial -> AppCompat 변경해서 액션바랑 기타 버튼들 색상이 변경되어있을겁니다.
 class MyAbility : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_myability)
+
+        //툴바에서 뒤로가기버튼 구현
+        var finish = findViewById<ImageButton>(R.id.BackButton)
+        finish.setOnClickListener{
+            Toast.makeText(this, "메인페이지로 돌아가기", Toast.LENGTH_SHORT).show()
+            finish()}
+
+
+        val next = findViewById<Button>(R.id.testbutton)
+        next.setOnClickListener{
+            val intent = Intent(this, MyAbilityDetail::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/android/iz4/MyAbilityDetail.kt
+++ app/src/main/java/com/android/iz4/MyAbilityDetail.kt
@@ -2,10 +2,17 @@ package com.android.iz4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.Toast
 
 class MyAbilityDetail : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_myabilitydetail)
+
+        var finish = findViewById<ImageButton>(R.id.BacktoMain)
+        finish.setOnClickListener{
+            Toast.makeText(this, "마이팀 메인페이지로 돌아가기", Toast.LENGTH_SHORT).show()
+            finish()}
     }
 }
