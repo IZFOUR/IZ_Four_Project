a84457b55dbb77ea507fc06fe702aa8eff23de6e
seongssu <loveforggong@gmail.com>
2023-08-14 21:10:53 +0900

    [FEAT] : ⚙️ MainActivity에서 버튼클릭 시 페이지 이동만 구현

--- app/src/main/java/com/android/iz4/MainActivity.kt
+++ app/src/main/java/com/android/iz4/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.android.iz4
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnpage = findViewById<Button>(R.id.mbtnpage)
+        val btnteam = findViewById<Button>(R.id.mbtnteam)
+
+        btnpage.setOnClickListener {
+            val intent = Intent(this, MyPage::class.java )
+            startActivity(intent)
+        }
+        btnteam.setOnClickListener {
+            val intent = Intent(this, MyAbility::class.java)
+            startActivity(intent)
+        }
+
     }
 }
