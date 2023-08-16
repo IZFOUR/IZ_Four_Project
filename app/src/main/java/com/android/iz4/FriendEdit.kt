cfe0f81753debc82f426a81a513630bd51618ad2
seongssu <loveforggong@gmail.com>
2023-08-16 16:45:11 +0900

    [FEAT] : ⚙️ activity_friendedit.xml 디자인 초안

--- app/src/main/java/com/android/iz4/FriendEdit.kt
+++ app/src/main/java/com/android/iz4/FriendEdit.kt
@@ -2,10 +2,17 @@ package com.android.iz4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class FriendEdit : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_friendedit)
+
+        val febtnback = findViewById<ImageButton>(R.id.febackbtn)
+        febtnback.setOnClickListener {
+            finish()
+        }
+
     }
 }
