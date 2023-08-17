b6c67a5ae2063acab0b7d1036c7497b658300550
cbh1992 <whgg9786@gmail.com>
2023-08-17 17:27:38 +0900

    [FEAT] : ⚙️ MyAbilityDetail페이지 일부 구현

--- app/src/main/java/com/android/iz4/MyAbilityDetail.kt
+++ app/src/main/java/com/android/iz4/MyAbilityDetail.kt
@@ -3,6 +3,7 @@ package com.android.iz4
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.ProgressBar
 import android.widget.Toast
@@ -12,16 +13,28 @@ class MyAbilityDetail : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_myabilitydetail)
 
-        var finish = findViewById<ImageButton>(R.id.BacktoMain)
-        finish.setOnClickListener{
-            Toast.makeText(this, "마이팀 메인페이지로 돌아가기", Toast.LENGTH_SHORT).show()
-            finish()}
 
         var ProcessPlus = findViewById<Button>(R.id.DetailUpdate)
         var ABarProcess = findViewById<ProgressBar>(R.id.ABarProcess)
+        var textarea = findViewById<EditText>(R.id.testarea)
         ProcessPlus.setOnClickListener{
             ABarProcess.incrementProgressBy(20)
+
+            //해당 코드 사용시 numberformatexception 오류발생
+            var texttest = ABarProcess.toString()
+            textarea.setText("texttest")
+
+            //var value = ABarProcess.toString()
+
+
+
         }
+
+
+
+        var finish = findViewById<ImageButton>(R.id.BacktoMain)
+        finish.setOnClickListener{
+            finish()}
     }
 
 
