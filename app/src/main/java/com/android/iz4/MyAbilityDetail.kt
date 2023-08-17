4b15628a0d2cf36f71a8f0a16b31ef9307a89efa
cbh1992 <whgg9786@gmail.com>
2023-08-17 23:05:28 +0900

    [FEAT] : ⚙️ MyAbilityDetail페이지 ProgressBar 수정

--- app/src/main/java/com/android/iz4/MyAbilityDetail.kt
+++ app/src/main/java/com/android/iz4/MyAbilityDetail.kt
@@ -1,12 +1,12 @@
 package com.android.iz4
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.ProgressBar
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MyAbilityDetail : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,17 +16,28 @@ class MyAbilityDetail : AppCompatActivity() {
 
         var ProcessPlus = findViewById<Button>(R.id.DetailUpdate)
         var ABarProcess = findViewById<ProgressBar>(R.id.ABarProcess)
-        var textarea = findViewById<EditText>(R.id.testarea)
+        var ABarDeadline = findViewById<ProgressBar>(R.id.ABarDeadLine)
+        var textarea = findViewById<EditText>(R.id.memoone)
+        var textarea2 = findViewById<EditText>(R.id.memotwo)
+
         ProcessPlus.setOnClickListener{
             ABarProcess.incrementProgressBy(20)
 
             //해당 코드 사용시 numberformatexception 오류발생
-            var texttest = ABarProcess.toString()
-            textarea.setText("texttest")
+            var texttest = ABarProcess.progress.toString()
+            var texttest2 = ABarDeadline.progress.toString()
+            textarea.setText(texttest)
 
             //var value = ABarProcess.toString()
 
-
+            var progressnum = texttest.toInt()
+            var progressnum2 = texttest2.toInt()
+            if(progressnum<progressnum2){
+                ABarDeadline.getProgressDrawable().setColorFilter(
+                    Color.RED, android.graphics.PorterDuff.Mode.SRC_IN);
+            }
+            else{ABarDeadline.getProgressDrawable().setColorFilter(
+                Color.BLUE, android.graphics.PorterDuff.Mode.SRC_IN);}
 
         }
 
