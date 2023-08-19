21fd269407cc27990a779f980450ed255a186c22
seongssu <loveforggong@gmail.com>
2023-08-20 01:17:22 +0900

    [CONFLICT] : 💥🚚 충돌 해결 대규모 수정(세부내용 내부에적음)

--- app/src/main/java/com/android/iz4/MyAbility.kt
+++ app/src/main/java/com/android/iz4/MyAbility.kt
@@ -1,20 +1,40 @@
 package com.android.iz4
 
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.ProgressBar
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 
 
 //(ViewBinding, Fragment, RecyclerView, Adapter는 사용하지 말아주세요.)
 //7777I에엙 그럼 뭘 사용해야하지
 class MyAbility : AppCompatActivity() {
+    lateinit var abilityresult: ActivityResultLauncher<Intent>
+
+    companion object{
+        val titleList = mutableListOf("팀A프로젝트", "팀B프로젝트", "팀C프로젝트")
+        var progressnumList = mutableListOf("0","0","0")
+        val deadlinenumList = mutableListOf("0","0","0")
+        val MemoList = mutableListOf("","","","","","","","","","")
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_myability)
+        Log.d("LifecycleMyAbility", "onCreate")
+
+        val next_teamList = mutableListOf<Button>(
+            findViewById(R.id.AtoDetail),
+            findViewById(R.id.BtoDetail),
+            findViewById(R.id.CtoDetail),
+        )
+
 
         //툴바에서 뒤로가기버튼 구현
         var finish = findViewById<ImageButton>(R.id.BackButton)
@@ -25,10 +45,110 @@ class MyAbility : AppCompatActivity() {
         var ABar = findViewById<ProgressBar>(R.id.ABarProcess)
 
         //디테일 페이지로 넘겨주기
-        val next = findViewById<Button>(R.id.AtoDetail)
-        next.setOnClickListener{
-            val intent = Intent(this, MyAbilityDetail::class.java)
-            startActivity(intent)
+
+        next_teamList.forEachIndexed { index,button ->
+            button.setOnClickListener {
+                val intent = Intent(this, MyAbilityDetail::class.java)
+                intent.putExtra("index", index)
+                intent.putExtra("abtitle", titleList[index])
+                intent.putExtra("abprogressnum", progressnumList[index])
+                intent.putExtra("abdeadline", deadlinenumList [index])
+                intent.putStringArrayListExtra("abmemoList", ArrayList(MemoList))
+                Log.d("LifecycleMyAbility",progressnumList[index])
+                abilityresult.launch(intent)
+            }
         }
+        abilityresult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if (it.resultCode == RESULT_OK) {
+                val data = it.data
+                if (data != null) {
+                    val index = data.getIntExtra("index", -1)
+                    if (index >= 0) {
+                        progressnumList[index] = data.getStringExtra("abdprogressnum") ?: ""
+//                        deadlinenumList[index] = data.getStringExtra("abddeadlinenum") ?: ""
+                        MemoList[index] = data.getStringExtra("abdmemoList") ?: ""
+                        progressnumList.add( progressnumList[index] ?: "")
+                        deadlinenumList.add( deadlinenumList[index] ?: "")
+                        Log.d("LifecycleMyAbility","넘어왔나? ${progressnumList[index]}")
+
+                        var ABarProcess = findViewById<ProgressBar>(R.id.ABarProcess)
+                        var ABarDeadline = findViewById<ProgressBar>(R.id.ABarDeadLine)
+                        var BBarProcess = findViewById<ProgressBar>(R.id.BBarProcess)
+                        var BBarDeadline = findViewById<ProgressBar>(R.id.BBarDeadLine)
+                        var CBarProcess = findViewById<ProgressBar>(R.id.CBarProcess)
+                        var CBarDeadline = findViewById<ProgressBar>(R.id.CBarDeadLine)
+                        var progressnum =  progressnumList[index].toInt()
+                        var progressnum2 = deadlinenumList[index].toInt()
+
+
+                        if (index == 0) {
+                            ABarProcess.progress = progressnum
+                            if(progressnum<progressnum2){
+                                ABarDeadline.getProgressDrawable().setColorFilter(
+                                    Color.RED, android.graphics.PorterDuff.Mode.SRC_IN)
+                            }else if(progressnum==progressnum2) {
+                                ABarDeadline.getProgressDrawable().setColorFilter(
+                                    Color.GREEN, android.graphics.PorterDuff.Mode.SRC_IN
+                                )
+                            }
+                            else{ABarDeadline.getProgressDrawable().setColorFilter(
+                                Color.BLUE, android.graphics.PorterDuff.Mode.SRC_IN)
+                            }
+                        } else if (index == 1) {
+                            BBarProcess.progress = progressnum
+                            if(progressnum<progressnum2){
+                                BBarDeadline.getProgressDrawable().setColorFilter(
+                                    Color.RED, android.graphics.PorterDuff.Mode.SRC_IN)
+                            }else if(progressnum==progressnum2) {
+                                BBarDeadline.getProgressDrawable().setColorFilter(
+                                    Color.GREEN, android.graphics.PorterDuff.Mode.SRC_IN
+                                )
+                            }
+                            else{BBarDeadline.getProgressDrawable().setColorFilter(
+                                Color.BLUE, android.graphics.PorterDuff.Mode.SRC_IN)
+                            }
+                        }else if (index == 2) {
+                            CBarProcess.progress = progressnum
+                            if(progressnum<progressnum2){
+                                CBarDeadline.getProgressDrawable().setColorFilter(
+                                    Color.RED, android.graphics.PorterDuff.Mode.SRC_IN)
+                            }else if(progressnum==progressnum2) {
+                                CBarDeadline.getProgressDrawable().setColorFilter(
+                                    Color.GREEN, android.graphics.PorterDuff.Mode.SRC_IN
+                                )
+                            }
+                            else{CBarDeadline.getProgressDrawable().setColorFilter(
+                                Color.BLUE, android.graphics.PorterDuff.Mode.SRC_IN)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+    override fun onStart() {
+        super.onStart()
+        Log.d("LifecycleMyAbility", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("LifecycleMyAbility", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("LifecycleMyAbility", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("LifecycleMyAbility", "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("LifecycleMyAbility", "onDestroy")
     }
 }
--- app/src/main/java/com/android/iz4/MyAbilityDetail.kt
+++ app/src/main/java/com/android/iz4/MyAbilityDetail.kt
@@ -1,9 +1,11 @@
 package com.android.iz4
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.content.SharedPreferences
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageButton
@@ -13,20 +15,21 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MyAbilityDetail : AppCompatActivity() {
+    var progressnum: Int = 0
+    var progressnum2: Int = 0
+    var texttest = ""
+    var texttest2 = ""
 
-    lateinit var pref: SharedPreferences
-    lateinit var editor: SharedPreferences.Editor
     @SuppressLint("SuspiciousIndentation")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_myabilitydetail)
 
-        //진행상황 부분
-        var ProcessPlus = findViewById<Button>(R.id.DetailUpdate)
-        var ABarProcess = findViewById<ProgressBar>(R.id.ABarProcess)
-        var ABarDeadline = findViewById<ProgressBar>(R.id.ABarDeadLine)
 
-        //메모부분
+        var ProcessPlus = findViewById<Button>(R.id.DetailUpdate)
+        var ABarProcess = findViewById<ProgressBar>(R.id.abd_ABarProcess)
+        var ABarDeadline = findViewById<ProgressBar>(R.id.abd_ABarDeadLine)
+
         var memosave = findViewById<Button>(R.id.memosave)
         var memoinput = findViewById<EditText>(R.id.memoinput)
         var memoone = findViewById<TextView>(R.id.memoone)
@@ -40,57 +43,95 @@ class MyAbilityDetail : AppCompatActivity() {
         var memonine = findViewById<TextView>(R.id.memonine)
         var memoten = findViewById<TextView>(R.id.memoten)
 
+        val text_title = findViewById<TextView>(R.id.abd_title)
+
+        val index = intent.getIntExtra("index", -1)
+        val title = intent.getStringExtra("abtitle") ?: ""
+        val mbd_progress = intent.getStringExtra("abprogressnum") ?: ""
+//        val mbd_deadline = intent.getStringExtra("abdeadline") ?:""
+        ABarProcess.progress = mbd_progress.toInt()
+//        ABarDeadline.progress =mbd_progress.toInt()
 
 
+        text_title.setText(title)
 
-        ProcessPlus.setOnClickListener{
+
+        ProcessPlus.setOnClickListener {
             ABarProcess.incrementProgressBy(20)
-            var texttest = ABarProcess.progress.toString()
-            var texttest2 = ABarDeadline.progress.toString()
 
-            var progressnum = texttest.toInt()
-            var progressnum2 = texttest2.toInt()
 
-            if(progressnum<progressnum2){
+            texttest = ABarProcess.progress.toString()
+            texttest2 = ABarDeadline.progress.toString()
+
+
+            progressnum = texttest.toInt()
+            progressnum2 = texttest2.toInt()
+            if (progressnum < progressnum2) {
                 ABarDeadline.getProgressDrawable().setColorFilter(
-                    Color.RED, android.graphics.PorterDuff.Mode.SRC_IN);
-            } else if(progressnum==progressnum2) {
+                    Color.RED, android.graphics.PorterDuff.Mode.SRC_IN
+                )
+            } else if (progressnum == progressnum2) {
                 ABarDeadline.getProgressDrawable().setColorFilter(
-                    Color.GREEN, android.graphics.PorterDuff.Mode.SRC_IN);
+                    Color.GREEN, android.graphics.PorterDuff.Mode.SRC_IN
+                )
+            } else {
+                ABarDeadline.getProgressDrawable().setColorFilter(
+                    Color.BLUE, android.graphics.PorterDuff.Mode.SRC_IN
+                )
             }
-            else{ABarDeadline.getProgressDrawable().setColorFilter(
-                Color.BLUE, android.graphics.PorterDuff.Mode.SRC_IN);}
+
 
         }
 
-        var index = 0
-        val textList = arrayOf(memoone,memotwo,memothree,memofour,memofive,memosix,memoseven,memoeight,memonine,memoten)
+        var memoIndex = 0
         memosave.setOnClickListener {
             var input = memoinput.text.toString()
+//            var one = memoone.text.toString()
+//            var two = memotwo.text.toString()
+//            var three = memothree.text.toString()
+//            var four = memofour.text.toString()
+//            var five = memofive.text.toString()
+//            var six = memosix.text.toString()
+//            var seven = memoseven.text.toString()
+//            var eight = memoeight.text.toString()
+//            var nine = memonine.text.toString()
+//            var ten = memoten.text.toString()
 
-
-            if (index<textList.size){
-                val textView = textList[index]
-                textView.text = input
-                index++
+            when (memoIndex) {
+                0 -> memoone.text = input
+                1 -> memotwo.text = input
+                2 -> memothree.text = input
+                3 -> memofour.text = input
+                4 -> memofive.text = input
+                5 -> memosix.text = input
+                6 -> memoseven.text = input
+                7 -> memoeight.text = input
+                8 -> memonine.text = input
+                9 -> memoten.text = input
             }
-            else {
-                Toast.makeText(this,"메모는 10개만 지원합니다",Toast.LENGTH_SHORT).show()
+            memoIndex++
+            if (memoIndex >= 10) {
+                memoIndex = 0
             }
-
-
-
         }
 
-        //MyAbility로 돌아가기
+
         var finish = findViewById<ImageButton>(R.id.BacktoMain)
-        finish.setOnClickListener{
-            finish()}
-    }
+        finish.setOnClickListener {
+            finish()
+        }
 
-    //이쯤에서 SharedPreferences 로 값 저장해주기
-    private fun savedata(){
+        val saveButton = findViewById<Button>(R.id.abd_savebutton)
+        saveButton.setOnClickListener {
 
+            val intent = Intent(this, MyAbility::class.java)
+            intent.putExtra("index", index)
+            intent.putExtra("abdprogressnum", texttest)
+            intent.putExtra("abddeadlinenum", texttest2)
+            Log.d("LifecycleMyAbility", texttest)
+            setResult(RESULT_OK, intent)
+            finish()
+        }
     }
 
 
